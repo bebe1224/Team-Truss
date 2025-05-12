789830aaf8973f92523794371f5a3afb8cc7abe7
bebe1224 <bebe1224@naver.com>
2025-05-12 18:13:38 +0900

    2025-5-12

--- script/header/header_fetch.js
+++ script/header/header_fetch.js
@@ -1,7 +1,23 @@
 
 // <div id="header-placeholder"></div>   이곳에 불러오는 방법ㅎㅎ
-fetch('../../components/header.html')
-    .then(response => response.text())
+// fetch('../../components/header.html')
+//     .then(response => response.text())
+//     .then(data => {
+//         document.getElementById('header-placeholder').innerHTML = data;
+//     })
+//     .catch(error => console.error('헤더 로드 실패:', error));
+
+
+const isLocal = location.hostname === 'localhost' || location.hostname === '127.0.0.1';
+const fetchPath = isLocal
+    ? '../../components/header.html'
+    : '/TEAM-TRUSS/components/header.html';
+
+fetch(fetchPath)
+    .then(response => {
+        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+        return response.text();
+    })
     .then(data => {
         document.getElementById('header-placeholder').innerHTML = data;
     })
