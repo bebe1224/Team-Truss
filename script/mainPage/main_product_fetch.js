95bad9579a7af0c9e49c4bfbff8de1a42121114f
Choi Jung Yun <bebe1224@naver.com>
2025-05-25 22:50:21 +0900

    대소문자 씨발

--- script/mainPage/main_product_fetch.js
+++ script/mainPage/main_product_fetch.js
@@ -1,6 +1,6 @@
 (() => {
     const isLocal = location.hostname === 'localhost' || location.hostname === '127.0.0.1';
-    const fetchPath = isLocal ? '../../components/product.html' : '/Team-Truss/components/Product.html';
+    const fetchPath = isLocal ? '../../components/product.html' : '/Team-Truss/components/product.html';
     const dataPath = isLocal ? '../../json/mainPage.json' : '/Team-Truss/json/mainPage.json';
 
     const maxPerContainer = 4;
